ff7df92776d26ad7994135fad4d43653967fd505
Thai Duong Vu <vu.t.duong01@gmail.com>
2019-12-31 11:07:39 -0500

    Import dependencies

--- app/src/main/java/com/thaiduong/currencyxchange/MainActivity.java
+++ app/src/main/java/com/thaiduong/currencyxchange/MainActivity.java
@@ -3,12 +3,17 @@ package com.thaiduong.currencyxchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = findViewById(R.id.spinner);
     }
 }
