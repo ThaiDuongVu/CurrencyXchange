1b7e1edd16f01e0f427f748a4019c4dd83769593
Thai Duong Vu <vu.t.duong01@gmail.com>
2019-12-31 11:29:23 -0500

    Add basic spinners

--- app/src/main/java/com/thaiduong/currencyxchange/MainActivity.java
+++ app/src/main/java/com/thaiduong/currencyxchange/MainActivity.java
@@ -3,17 +3,42 @@ package com.thaiduong.currencyxchange;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Spinner spinner;
+    private ArrayList<String> currencyList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        spinner = findViewById(R.id.spinner);
+        Spinner fromSpinner = findViewById(R.id.fromSpinner);
+        Spinner toSpinner = findViewById(R.id.toSpinner);
+
+        spinnerHandler(fromSpinner);
+        spinnerHandler(toSpinner);
+    }
+
+    private void spinnerHandler(Spinner spinner) {
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, currencyList);
+        spinner.setAdapter(arrayAdapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
