ef6019d648b9aad208770b78304453a473259b69
Thai Duong Vu <vu.t.duong01@gmail.com>
2019-12-31 14:56:24 -0500

    Add widgets to layout

--- app/src/main/java/com/thaiduong/currencyxchange/MainActivity.java
+++ app/src/main/java/com/thaiduong/currencyxchange/MainActivity.java
@@ -9,18 +9,76 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private ArrayList<String> currencyList = new ArrayList<>();
+
+    private Spinner fromSpinner;
+    private Spinner toSpinner;
+
+    private String[] currencies = {
+            "USD",
+            "AED",
+            "ARS",
+            "AUD",
+            "BGN",
+            "BRL",
+            "BSD",
+            "CAD",
+            "CHF",
+            "CLP",
+            "CNY",
+            "COP",
+            "CZK",
+            "DKK",
+            "DOP",
+            "EGP",
+            "EUR",
+            "FJD",
+            "GBP",
+            "GTQ",
+            "HKD",
+            "HRK",
+            "HUF",
+            "IDR",
+            "ILS",
+            "INR",
+            "ISK",
+            "JPY",
+            "KRW",
+            "KZT",
+            "MXN",
+            "MYR",
+            "NOK",
+            "NZD",
+            "PAB",
+            "PEN",
+            "PHP",
+            "PKR",
+            "PLN",
+            "PYG",
+            "RON",
+            "RUB",
+            "SAR",
+            "SEK",
+            "SGD",
+            "THB",
+            "TRY",
+            "TWD",
+            "UAH",
+            "UYU",
+            "VND",
+            "ZAR"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Spinner fromSpinner = findViewById(R.id.fromSpinner);
-        Spinner toSpinner = findViewById(R.id.toSpinner);
+        initialize();
 
         spinnerHandler(fromSpinner);
         spinnerHandler(toSpinner);
@@ -40,5 +98,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void initialize() {
+        fromSpinner = findViewById(R.id.fromSpinner);
+        toSpinner = findViewById(R.id.toSpinner);
+        currencyList.addAll(Arrays.asList(currencies));
     }
 }
